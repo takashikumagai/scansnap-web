226d9ee8ce44e3b9df8b284f6cf6209ab460f116
Takashi Kumagai <takashi.kumagai@example.com>
2025-04-22 21:36:11 +0900

    Updated template file

--- scansnapweb/scansnapwebapp/urls.py
+++ scansnapweb/scansnapwebapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello/', views.hello, name='hello'),
+    path("hello/", views.hello, name="hello"),
+    path("", views.home, name="home"),
 ]
--- scansnapweb/scansnapwebapp/views.py
+++ scansnapweb/scansnapwebapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def hello(request):
     return JsonResponse({"message": "hello"})
+
+def home(request):
+    return render(request, "main.html", context={})
