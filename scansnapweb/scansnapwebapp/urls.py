db7d31a001b9839bb5af2627278041ab19f26609
Takashi Kumagai <takashi.kumagai@example.com>
2025-05-03 18:55:52 +0900

    added user login and registration

--- scansnapweb/scansnapwebapp/urls.py
+++ scansnapweb/scansnapwebapp/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
+    # auth
+    path("login/", views.scansnapweb_login, name="scansnapweb-login"),
+    path("register/", views.register, name="register"),
+
+    # scan
     path("hello/", views.hello, name="hello"),
     path("", views.home, name="home"),
     path("get-scanner-info/", views.get_scanner_info, name="get-scanner-info"),
--- scansnapweb/scansnapwebapp/views.py
+++ scansnapweb/scansnapwebapp/views.py
@@ -3,13 +3,53 @@ import logging
 from pathlib import Path
 
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
 
 from . import utils
+from .forms import CreateUserForm
+
+
+def register(request):
+    form = CreateUserForm()
+
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("scansnapweb-login")
+
+    context = {"registration_form": form}
+    return render(request, "registration/register.html", context=context)
+
+
+def scansnapweb_login(request):
+    if request.user.is_authenticated:
+        return redirect("home")  # or wherever you want
+
+    form = AuthenticationForm()
+
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        # Note that authenticate() is used under the hood by form.is_valid()
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("home")  # or whatever success URL
+        else:
+            messages.error(request, "Invalid username or password.")
+
+    return render(request, "registration/login.html", {"form": form})
+
 
 def hello(request):
     return JsonResponse({"message": "hello"})
 
+
+@login_required(login_url="/login/")
 def home(request):
     return render(request, "scansnapwebapp/main.html", context={})
 
