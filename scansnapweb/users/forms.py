5fc7b4a39462e60acbf273a58b50f3c6e2a2727e
Takashi Kumagai <takashi.kumagai@example.com>
2025-05-04 17:53:16 +0900

    added favicon + styling

--- scansnapweb/users/forms.py
+++ scansnapweb/users/forms.py
@@ -3,6 +3,24 @@ from django.contrib.auth.models import User
 
 
 class CreateUserForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({
+            "class": "w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500",
+            "placeholder": "Your username"
+        })
+        self.fields['email'].widget.attrs.update({
+            "class": "w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500",
+            "placeholder": "Your email"
+        })
+        self.fields['password1'].widget.attrs.update({
+            "class": "w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500",
+            "placeholder": "Password"
+        })
+        self.fields['password2'].widget.attrs.update({
+            "class": "w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500",
+            "placeholder": "Confirm your password"
+        })
 
     class Meta:
 
